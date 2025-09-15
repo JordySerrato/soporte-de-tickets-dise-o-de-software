5e31746fb06894e3b467af94619a1a69afe3a50a
AldoBlink789 <76199537@certus.edu.pe>
2025-09-15 07:26:22 -0500

    fsdf

--- registro.js
+++ registro.js
@@ -12,12 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 <input type="text" id="organizacion" placeholder="Organización/Empresa" required>
             `;
         } 
-        else if (tipoUsuario.value === "admin") {
-            extraCampos.innerHTML = `
-                <input type="text" id="organizacion" placeholder="Organización/Empresa" required>
-                <input type="password" id="claveAdmin" placeholder="Clave de administrador" required>
-            `;
-        } 
         else if (tipoUsuario.value === "tecnico") {
             extraCampos.innerHTML = `
                 <input type="text" id="organizacion" placeholder="Organización/Empresa" required>
@@ -38,11 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (!nombre || !correo || !clave || !tipoUsuario.value) {
             mostrarError("Por favor, completa todos los campos.");
-            return;
-        }
-
-        if (tipoUsuario.value === "admin" && document.getElementById("claveAdmin").value !== "admin123") {
-            mostrarError("Clave de administrador incorrecta.");
             return;
         }
 
